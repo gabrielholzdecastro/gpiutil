d24707c63a9d750f17c2dad5d9d86d3ac9a105bb
gabriel.holz <gabriel.castro@el.com.br>
2022-01-17 21:40:15 -0300

    first commit

---
+++ backend/src/main/java/com/el/gpiutils/GpiutilsApplication.java
@@ -0,0 +1,13 @@
+package com.el.gpiutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpiutilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpiutilsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/el/gpiutils/GpiutilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.el.gpiutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GpiutilsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
